188e61e8247fde4dbcb3f5df704e4c330908b8cc
Unseencoderz <2k22.cse.2213591@gmail.com>
2024-10-12 00:18:56 +0530

    update cors

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ const forgotRoutes = require("./routes/forgot");
 connection();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://studentexplorer.netlify.app', // Frontend URL
+    methods: 'GET,POST,PUT,DELETE', // Specify allowed HTTP methods
+    credentials: true, // Allow credentials like cookies, etc.
+  }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
